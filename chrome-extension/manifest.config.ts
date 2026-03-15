7d4441ea53e8c3313e7250e81ba8cefe21837edd
Hugo Boyano <hnboyano@gmail.com>
2026-03-16 00:21:10 +0100

    update manifest

--- chrome-extension/manifest.config.ts
+++ chrome-extension/manifest.config.ts
@@ -3,6 +3,12 @@ import pkg from './package.json'
 
 const IS_PROD = process.env.NODE_ENV === 'production'
 
+const host_permissions = ['https://*.hgo.one/*']
+
+if (!IS_PROD) {
+  host_permissions.push('http://localhost:8080/*')
+}
+
 export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
@@ -20,9 +26,6 @@ export default defineManifest({
   side_panel: {
     default_path: 'src/sidepanel/index.html'
   },
-  host_permissions: [
-    'https://*.hgo.one/*',
-    IS_PROD ? '' : 'http://localhost:8080/*'
-  ],
+  host_permissions,
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArwYJv7Jxbsa77/9x4nGvSWa3w/YNTpST8gM2ghbRsur8WMiHA3SzcPD1c4KttN1ojD12Er31jSvXpbU94dNQgqxIhBww0OPCPVabtP43Az/UPMtAEsIKCoJs3bKi3f+G3pDfXn6h+KuAv0KZ8Fk6mxMFWqUNgn9W7Cr67sObgHCzY44YnMRUU+7AX1qVd/nerSZEbLJmE6v0cBY+XzSLoK1RQMp1cNZXW4GKegyYMZp8SspfhNqkyogUGZCas44jo2G9c0CugyIrLEdP/qKcjYPJ9wMsyxQSZiW46aXepl5QV0A2LLzYub1dWHMVm58zRjESK7Oo4XtB5qzDTK+VNQIDAQAB'
 })
